95bb71fa72f14e34d80a53856a9775effa45ef29
Darin <drnellman@gmail.com>
2020-02-18 11:03:45 +0200

    django3 class - lecture 61 - creating a user

--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
 
 # Create your views here.
 def signupuser(request):
-
-    return render(request, 'todo/signupuser.html', {'form':UserCreationForm()})
+    if request.method == 'GET':
+        return render(request, 'todo/signupuser.html', {'form':UserCreationForm()})
+    else:
+        #create a new user
+        #note - you can only 'POST' info through a form
+        #every time you request a page by just putting a url in the browser is a 'GET'
+        if request.POST['password1'] == request.POST['password2']:
+            user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+            user.save()
+        else:
+            #tell the user that the passwords didn't match
+            print("hello")
