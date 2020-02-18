cdf928b72bd7b38c79a39cae7adac54360a8ca3d
Darin <drnellman@gmail.com>
2020-02-18 15:52:20 +0200

    after lecture 65

--- todo/views.py
+++ todo/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.contrib.auth import login, logout, authenticate
+
+def home(request):
+    return render(request, 'todo/home.html')
 
 
-# Create your views here.
 def signupuser(request):
     if request.method == 'GET':
         return render(request, 'todo/signupuser.html', {'form':UserCreationForm()})
@@ -12,8 +16,37 @@ def signupuser(request):
         #note - you can only 'POST' info through a form
         #every time you request a page by just putting a url in the browser is a 'GET'
         if request.POST['password1'] == request.POST['password2']:
-            user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
-            user.save()
+            try:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('currenttodos')
+            except IntegrityError:
+                return render(request, 'todo/signupuser.html', {'form':UserCreationForm(), 'error':'Sorry, That username has already been taken. Please try a different username'})
         else:
             #tell the user that the passwords didn't match
-            print("hello")
+                    return render(request, 'todo/signupuser.html', {'form':UserCreationForm(), 'error':'Passwords did not match'})
+
+def loginuser(request):
+    if request.method == 'GET':
+        return render(request, 'todo/loginuser.html', {'form':AuthenticationForm()})
+    else:
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, "todo/loginuser.html", {'form':AuthenticationForm(), 'error':'Username and password did not match our records'})
+        else:
+            login(request, user)
+            return redirect('currenttodos')
+
+
+
+def logoutuser(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('home')
+
+
+
+
+def currenttodos(request):
+    return render(request, 'todo/currenttodos.html')
--- todowoo/urls.py
+++ todowoo/urls.py
@@ -21,8 +21,11 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     #auth
-    path('signup/', views.signupuser, name='signupuser')
+    path('signup/', views.signupuser, name='signupuser'),
+    path('login/', views.loginuser, name='loginuser'),
+    path('logout/', views.logoutuser, name='logoutuser'),
 
     #todos
-
+    path('', views.home, name='home'),
+    path('current/', views.currenttodos, name='currenttodos'),
 ]
